cc672b97774bbd640a8a92292d95cb2db76dd453
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-02-27 10:31:29 +0000

    Example data packet

---
+++ src/malaria_atlas/README.md
@@ -0,0 +1,1 @@
+* `data/National_Unit-data.csv` Downloaded from Malaria Atlas on 2024-02-27
---
+++ src/malaria_atlas/orderly.R
@@ -0,0 +1,3 @@
+orderly2::orderly_resource("data/National_Unit-data.csv")
+orderly2::orderly_artefact("Malaria Atlas Data",
+                           "data/National_Unit-data.csv")
