60ef2ceac0538ba6d3d3fcd6a568239b9f3a7bcd
Peter Winskill <p.winskill@imperial.ac.uk>
2024-05-15 11:52:07 +0100

    Update urbanisation prediction to be more robust

--- src/population/population_utils.R
+++ src/population/population_utils.R
@@ -1,36 +1,10 @@
+# Define logistic function
 logistic_function <- function(year, B, C) {
-  year <- year - 2030
-  0.8 / (1 + exp(-B * (year - C)))
+  1 / (1 + exp(-B * (year - C)))
 }
 
-# extrapolate_logistic <- function(year, proportion_urban, threshold_year) {
-#   
-#   data <- data.frame(
-#     year = year[year <= threshold_year & year >= 2030],
-#     proportion_urban = proportion_urban[year <= threshold_year & year >= 2030]
-#   )
-#   # Fit a polynomial model
-#   fit <- nls(
-#     proportion_urban ~ logistic_function(year, B, C), 
-#     data = data,
-#     start = list(B = 0.01, C = mean(data$proportion_urban))
-#   )
-#   
-#   # Create a sequence for prediction
-#   predictions <- predict(fit, newdata = data.frame(year = year[year > threshold_year]))
-#   
-#   out <- c(proportion_urban[year <= threshold_year], predictions)
-#   
-#   return(out)
-# }
-
 extrapolate_logistic <- function(year, proportion_urban, threshold_year) {
-  
-  # Define logistic function
-  logistic_function <- function(year, B, C) {
-    1 / (1 + exp(-B * (year - C)))
-  }
-  
+
   # Filter data for fitting
   data <- data.frame(
     year = year[year <= threshold_year & year >= 2030],
@@ -51,7 +25,7 @@ extrapolate_logistic <- function(year, proportion_urban, threshold_year) {
   upper_bounds <- c(B = 1, C = max(data$year))
   
   # Fit a polynomial model with error handling
-  fit <- tryCatch({
+  fit <- 
     nls(
       proportion_urban ~ logistic_function(year, B, C), 
       data = data,
@@ -60,17 +34,7 @@ extrapolate_logistic <- function(year, proportion_urban, threshold_year) {
       upper = upper_bounds,
       algorithm = "port"  # Use the 'port' algorithm for bounded parameters
     )
-  }, error = function(e) {
-    warning("NLS fitting failed: ", e$message)
-    return(NULL)
-  })
-  
-  # If fitting fails, return NA or a reasonable default
-  if (is.null(fit)) {
-    stop("Fail")
-    return(rep(NA, length(year)))
-  }
-  
+
   # Create a sequence for prediction
   predictions <- predict(fit, newdata = data.frame(year = year[year > threshold_year]))
   
