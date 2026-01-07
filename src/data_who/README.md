7860fad25937f9fe46c8003af78763145b20cfd0
Peter Winskill <p.winskill@imperial.ac.uk>
2026-01-07 13:46:47 +0000

    Update with 2025 WMR data

--- src/data_who/README.md
+++ src/data_who/README.md
@@ -1,43 +1,50 @@
 **Files**
 
-`data/itn_delivered.csv`
+`wmr_annex_4g.csv`
 
 **Source**
 
-ITNs delivered as reported in the WMR (annex4D)
-<https://cdn.who.int/media/docs/default-source/malaria/world-malaria-reports/wmr2024_annex_4d.xlsx?sfvrsn=f2529a46_3>
+Commodities distribution and coverage for malaria endemic countries and areas, 2022–2024
+from the 2025 WMR (annex4g)
+<https://cdn.who.int/media/docs/default-source/malaria/world-malaria-reports/wmr2025_annex_4g.xlsx?sfvrsn=77f12a71_6>
 
 **Download date**
 
-05/03/2024
+06/01/2026
 
 ---
 
 **Files**
 
-`data/irs_people_protected.csv`
+`wmr_annex_4h.csv`
 
 **Source**
 
-People protected by IRS as reported in the WMR (annex4D)
-<https://www.who.int/teams/global-malaria-programme/reports/world-malaria-report-2023>
+Population denominator for case incidence and mortality rate, and estimated malaria cases and deaths, 2000–2024
+from the 2025 WMR (annex4h)
+<https://cdn.who.int/media/docs/default-source/malaria/world-malaria-reports/wmr2025_annex_4h.xlsx?sfvrsn=723cb792_4>
 
 **Download date**
 
-05/03/2024
+06/01/2026
 
 ---
 
 **Files**
 
-`data/wmr_cases_deaths.csv`
+`itns_delivered_archival.csv`
 
 **Source**
 
-Country estimates of malaria cases and deaths (annex4F)
-<https://cdn.who.int/media/docs/default-source/malaria/world-malaria-reports/wmr2024_annex_4f.xlsx?sfvrsn=1a59a8ac_4>
+Archival ITN delivery estimates from previous WMRs
 
-**Download date**
+---
 
-05/03/2024
+**Files**
+
+`irs_people_protected_archival.csv`
+
+**Source**
+
+Archival IRS people protected from previous WMRs
 
--- src/data_who/data_who.R
+++ src/data_who/data_who.R
@@ -1,2 +1,111 @@
-orderly::orderly_resource("data/")
+orderly::orderly_resource("data")
+orderly::orderly_resource("data_site-2601/wmr_annex_4g.csv")
+orderly::orderly_resource("data_site-2601/wmr_annex_4h.csv")
+orderly::orderly_resource("data_site-2601/itns_delivered_archival.csv")
+orderly::orderly_resource("data_site-2601/irs_people_protected_archival.csv")
 orderly::orderly_resource("README.md")
+
+
+orderly::orderly_artefact(
+  description = "WMR cases, deaths and PAR",
+  files = "wmr_cases_deaths.csv"
+)
+orderly::orderly_artefact(
+  description = "WMR ITNs distributed",
+  files = "wmr_itns_distributed.csv"
+)
+orderly::orderly_artefact(
+  description = "WMR ITNs distributed interpolation plot",
+  files = "wmr_itns_distributed.png"
+)
+orderly::orderly_artefact(
+  description = "WMR IRS people protected",
+  files = "wmr_irs_people_protected.csv"
+)
+orderly::orderly_artefact(
+  description = "WMR IRS people protected interpolation plot",
+  files = "wmr_irs_people_protected.png"
+)
+
+# Cases and deaths -------------------------------------------------------------
+wmr_cases_deaths <- read.csv("data_site-2601/wmr_annex_4h.csv") |>
+  tidyr::pivot_wider(
+    id_cols = c("region", "iso", "country", "year"),
+    names_from = "indicator",
+    values_from = "value"
+    ) |>
+  dplyr::rename(
+    wmr_par = "Population at risk",
+    wmr_cases_l = "Cases Lower",
+    wmr_cases = "Cases Point",
+    wmr_cases_u = "Cases Upper",
+    wmr_deaths_l = "Deaths Lower",
+    wmr_deaths = "Deaths Point",
+    wmr_deaths_u = "Deaths Upper"
+  )
+write.csv(wmr_cases_deaths, "wmr_cases_deaths.csv", row.names = FALSE)
+# ------------------------------------------------------------------------------
+
+wmr_int <- read.csv("data_site-2601/wmr_annex_4g.csv")  |>
+  dplyr::rename("iso3c" = "iso") |>
+  dplyr::mutate(value = as.numeric(value))
+
+# IRS people protected ---------------------------------------------------------
+irs_people_protected_wmr <- wmr_int |>
+  dplyr::filter(indicator == "Number of people protected by IRS") |>
+  dplyr::select("iso3c", "year", "value")
+
+irs_people_protected <- read.csv("data_site-2601/irs_people_protected_archival.csv") |>
+  dplyr::full_join(irs_people_protected_wmr, by = c("iso3c", "year")) |>
+  dplyr::mutate(
+    irs_people_protected = ifelse(!is.na(value), value, irs_people_protected),
+    irs_interpolated = irs_people_protected
+  ) |>
+  dplyr::select(-value) |>
+  dplyr::group_by(iso3c) |>
+  tidyr::complete(year) |>
+  dplyr::arrange(year) |>
+  tidyr::fill(irs_interpolated, .direction = "down") |>
+  dplyr::ungroup()
+
+irs_people_protected_plot <- ggplot2::ggplot(irs_people_protected) +
+  ggplot2::geom_line(ggplot2::aes(x = year, y = irs_interpolated / 1e6)) +
+  ggplot2::geom_point(ggplot2::aes(x = year, y = irs_people_protected / 1e6)) +
+  ggplot2::facet_wrap(~iso3c, scale = "free_y") +
+  ggplot2::ylab("IRS people protected (M)") +
+  ggplot2::xlab("Year") +
+  ggplot2::theme_bw()
+
+write.csv(irs_people_protected, "wmr_irs_people_protected.csv", row.names = FALSE)
+ggsave("wmr_irs_people_protected.png", irs_people_protected_plot, height = 15, width = 20)
+# ------------------------------------------------------------------------------
+
+# ITNs distributed -------------------------------------------------------------
+itns_distributed_wm <- wmr_int |>
+  dplyr::filter(indicator == "Number of ITNs distributed") |>
+  dplyr::select("iso3c", "year", "value")
+
+itns_distributed <- read.csv("data_site-2601/itns_delivered_archival.csv") |>
+  dplyr::full_join(itns_distributed_wmr, by = c("iso3c", "year")) |>
+  dplyr::mutate(
+    itns_distributed = ifelse(!is.na(value), value, itns_distributed),
+    itns_interpolated = itns_distributed
+  ) |>
+  dplyr::select(-value) |>
+  dplyr::group_by(iso3c) |>
+  tidyr::complete(year) |>
+  dplyr::arrange(year) |>
+  tidyr::fill(itns_interpolated, .direction = "down") |>
+  dplyr::ungroup()
+
+itns_distributed_plot <- ggplot2::ggplot(itns_distributed) +
+  ggplot2::geom_line(ggplot2::aes(x = year, y = itns_interpolated / 1e6)) +
+  ggplot2::geom_point(ggplot2::aes(x = year, y = itns_distributed / 1e6)) +
+  ggplot2::facet_wrap(~iso3c, scale = "free_y") +
+  ggplot2::ylab("ITNs delivered (M)") +
+  ggplot2::xlab("Year") +
+  ggplot2::theme_bw()
+
+write.csv(itns_distributed, "wmr_itns_distributed.csv", row.names = FALSE)
+ggsave("wmr_itns_distributed.png", itns_distributed_plot, height = 15, width = 20)
+# ------------------------------------------------------------------------------
